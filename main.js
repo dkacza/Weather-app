b2cc40ccdb4f6706efb9ea91bbef7d6b7d7f27ed
Dawid Kacza <dawidkacza02@gmail.com>
2022-11-26 19:58:59 +0100

    Reformated code. Created class for weather record and encapsulated URL generation into separate function.

--- main.js
+++ main.js
@@ -6,49 +6,78 @@
 // Current weather
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
-const geolocateBtn = document.querySelector('.geolocate-btn');
+// Forecast
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
-const geolocate = function() {
+class WeatherData {
+    constructor(main, desc, id, icon, temp, hum, wind, pres, clouds) {
+        this.main = main;
+        this.description = desc;
+        this.weatherId = id;
+        this.iconId = icon;
+
+        this.temp = {
+            celcius: temp - 273.15,
+            fahrenheit: (9 / 5) * (temp - 273.15) + 32,
+        };
+        (this.humidity = hum),
+            (this.windSpeed = wind),
+            (this.pressure = pres),
+            (this.cloudsPercentage = clouds);
+    }
+}
+
+const geolocate = function () {
     return new Promise((res, rej) => {
         navigator.geolocation.getCurrentPosition(res, rej);
-    })
-}
-const getCity = async function() {
-    const location = await geolocate();
-    const {latitude: lat, longitude: lng} = location.coords;
-    
-    const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lng}&limit=1&appid=b48fc9a3f6a6deaf3d65d0d6620f1a13`
+    });
+};
 
+const prepareURL = async function (requestType) {
+    const location = await geolocate();
+    const {latitude: lat, longitude: lon} = location.coords;
+    const weatherKey = 'b48fc9a3f6a6deaf3d65d0d6620f1a13';
+
+    switch (requestType) {
+        case 'reverseGeo':
+            return `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${weatherKey}`;
+        case 'current':
+            return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherKey}`;
+        case 'forecast':
+            return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherKey}`;
+        default:
+            throw new Error('Wrong request type');
+    }
+};
+
+const getCity = async function () {
+    const url = await prepareURL('reverseGeo');
     const [response] = await fetch(url).then(res => res.json());
 
-    const resultString = `${response.name}, ${response.country}`
+    const resultString = `${response.name}, ${response.country}`;
     return resultString;
-}
-const getCurrentWeather = async function() {
-    const location = await geolocate();
-    const {latitude: lat, longitude: lng} = location.coords;
+};
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=b48fc9a3f6a6deaf3d65d0d6620f1a13`;
+const getCurrentWeather = async function () {
+    try {
+        const url = await prepareURL('current');
+        const response = await fetch(url).then(res => res.json());
 
-    const response = await fetch(url).then(res => res.json());
-    console.log(response);
+        const currentWeather = new WeatherData(
+            response.weather[0].main,
+            response.weather[0].description,
+            response.weather[0].id,
+            response.weather[0].icon,
+            response.main.temp,
+            response.main.humidity,
+            response.wind.speed,
+            response.main.pressure,
+            response.clouds.all
+        );
+        return currentWeather;
+    } catch (err) {
+        throw new Error(err);
+    }
+};
 
-    // Values
-    const temperature = response.main.temp; // K
-    const humidity = response.main.humidity; // %
-    const pressure = response.main.pressure; // hPa
-    const wind = response.wind.speed; // m/s
-    const clouds = response.clouds.all // %
-
-    // Desctiptions
-    const description = response.weather[0].description; // rain, snow
-    const main = response.weather[0].main;
-    const icon = response.weather[0].icon;
-    const weatherID = response.weather[0].id
-
-    console.log(temperature, humidity, pressure, wind, clouds);
-    console.log(weatherID, icon, main, description);
-}
-
-getCity().then(res => console.log(res));
-getCurrentWeather();
+getCurrentWeather().then(response => console.log(response)).catch(err => console.error(err));
