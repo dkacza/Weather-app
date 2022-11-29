5a850f823ea07f742b7298df1e65ab1ed75aaed9
Dawid Kacza <dawidkacza02@gmail.com>
2022-11-29 19:10:50 +0100

    Refactored and fixed code after merge Forecast section is ready to be developed. Request overwriting must be fixed. fixed

--- main.js
+++ main.js
@@ -123,10 +123,13 @@ const getCurrentWeather = async function (location) {
 };
 
 const dom = {
+    // Main container and message display
     mainContainer: document.querySelector('div.container'),
+    userMsg: document.querySelector('div.msg'),
 
     // Search section inputs.
     cityInput: document.querySelector('input.searchbar'),
+    cityInputWrapper: document.querySelector('div.input-wrapper'),
     searchBtn: document.querySelector('button.search-btn'),
     geolocateBtn: document.querySelector('button.geolocate-btn'),
 
@@ -163,44 +166,87 @@ let curWeather;
 
 const handleGeo = async function() {
     try {
+        // Hide message and show the spinner
         dom.mainContainer.classList.add('loading');
+        dom.mainContainer.classList.remove('disp-msg')
+
         const location = await getCurrentLocation();
         dom.updateLocation(location);
-
         curWeather = await getCurrentWeather(location);
+
+        // Display response and hide the spinner
         dom.updateWeather(curWeather);
         dom.mainContainer.classList.remove('loading');
+        dom.cityInputWrapper.classList.remove('error');
+
+        initiialState = false;
     } catch (err) {
         console.error(err);
+
+        // Hide spinner and display geolocation message in the center of the sceen
+        dom.userMsg.textContent = 'Turn on the geolocation and then try again'
+        dom.mainContainer.classList.add('disp-msg');
+        dom.mainContainer.classList.remove('loading');
     }
 }
 const handleQuerry = async function() {
     try {
+        // Hide message and show the spinner
         dom.mainContainer.classList.add('loading');
-        const locationQuerry = dom.cityInput.value;
-        if (!locationQuerry) return;
-        const location = await getLocationFromQuery(locationQuerry);
-        dom.updateLocation(location);
+        dom.mainContainer.classList.remove('disp-msg');
 
+        const locationQuerry = dom.cityInput.value;
+        if (!locationQuerry) {
+            dom.mainContainer.classList.remove('loading');
+            return;
+        }
+        const location = await getLocationFromQuery(locationQuerry);
+
+        // Display location and remove the error from under the searchbar
+        dom.cityInputWrapper.classList.remove('error');
+        dom.updateLocation(location);
         curWeather = await getCurrentWeather(location);
         dom.updateWeather(curWeather);
+
+        // Clear the searchbar and hide the spinner if everything goes fine
         dom.cityInput.value = '';
         dom.mainContainer.classList.remove('loading');
+        dom.mainContainer.classList.remove('disp-msg')
+
+        initiialState = false;
     } catch (err) {
         console.error(err);
+        dom.cityInput.value = '';
+
+        // Hide spinner and display error under the searchbar
+        if (initiialState) {
+            dom.mainContainer.classList.add('disp-msg')
+        }
+        dom.cityInputWrapper.classList.add('error');
+        dom.mainContainer.classList.remove('loading');
     }
 }
 
-dom.geolocateBtn.addEventListener('click', handleGeo);
-dom.searchBtn.addEventListener('click', handleQuerry);
+// Toggling temperature
 dom.celcius.addEventListener('click', () => {
     dom.celciusSelected = true;
+    dom.celcius.classList.add('active');
+    dom.fahrenheit.classList.remove('active');
     dom.updateWeather(curWeather);
 });
 dom.fahrenheit.addEventListener('click', () => {
     dom.celciusSelected = false;
+    dom.fahrenheit.classList.add('active');
+    dom.celcius.classList.remove('active');
     dom.updateWeather(curWeather);
 })
 
-dom.mainContainer.classList.add('loading');
-handleGeo();
+// Handling main events
+dom.geolocateBtn.addEventListener('click', handleGeo);
+dom.searchBtn.addEventListener('click', handleQuerry);
+document.onkeydown = (e) => {
+    if(e.key === 'Enter' && dom.cityInput.matches(':focus')) handleQuerry();
+}
+// Variable for a special first run of of handleQuerry with wrong city name
+// Without it, the program displays placeholder values instead of error messages
+let initiialState = true;
